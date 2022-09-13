890a8f2a266e9b612914d5778099f75bba1f5af1
Prince Stephen <Toughprince8968@gmail.com>
2022-09-12 20:22:00 -0700

    uyu

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of three digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+	int f_d;
+	int m_d;
+	int i_d;
+
+	while (c <= 999)
+	{
+		f_d = (c / 100 + '0');
+		m_d = (c / 10 % 10 + '0');
+		i_d = (c % 10 + '0');
+
+		if ((f_d < m_d) && (m_d < i_d))
+		{
+			putchar(f_d);
+			putchar(m_d);
+			putchara(i_d);
+
+			if (c != 789)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
