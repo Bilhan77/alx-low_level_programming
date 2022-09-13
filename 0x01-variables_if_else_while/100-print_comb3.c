aa544bb27651a622b63642f02461ca55097b1620
Prince Stephen <Toughprince8968@gmail.com>
2022-09-12 20:11:55 -0700

    lkj

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+	int f_d;
+	int i_d;
+
+	while (c <= 99)
+	{
+		f_d = (c / 10 + '0');
+		i_d = (c % 10 + '0');
+
+		if (f_d < i_d)
+		{
+			putchar(f_d);
+			putchar(i_d);
+
+			if (c != 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
