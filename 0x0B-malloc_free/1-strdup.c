3889075b3444464b5daa30ba4edda9327e6e4acc
Prince Stephen <Toughprince8968@gmail.com>
2022-10-03 12:44:11 -0700

    jio

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,11 +2,11 @@
 
 /**
  * _strdup - returns a pointer to a newly allocate
- * space in memory, which contains a copy of the 
+ * space in memory, which contains a copy of the
  * string given as a parameter.
  * @str: string to be copied
  *
- * Return: NULL is case of error, pointer to allocated 
+ * Return: NULL is case of error, pointer to allocated
  * space
  */
 
